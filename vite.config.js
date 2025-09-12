1a8c9255dd3056642933dc6bb2cf1663736ba818
Rémy Sabliet <sabliet@gmail.com>
2025-09-12 11:16:56 +0200

    remplace npm by pnpm lock and update title on home page (full stack dev)

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 3000,
+    },
     outDir: 'build',
     resolve: {
         alias: {
